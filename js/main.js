34489ac628f9871b373625809da09647bd88dcde
artremRemen <arthuremen@gmail.com>
2021-11-19 23:59:21 +0100

    execriceComplete

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
 const mainTitle = document.querySelector('h1');
 let result;
-
-const createLi = (cssClass, text)=>{
-    let element = document.createElement('li');
-    element.setAttribute('class',cssClass);
-    element.textContent= text;
-    element.insertAdjacentElement("afterbegin",grid);
+let element;
+let start = 100;
+let down = 120;
+const verifNbre = (start, stop)=>{
+    for (let i = start; i < stop; i++) {
+        if (i%3 === 0 && i%5 === 0) {
+            result = 'fizzbuzz';
+            createLi('fizzbuzz', result);
+        }
+        else if (i%3 === 0) {
+            result = 'fizz';
+            createLi('fizz', result);
+        }
+        else if (i%5 === 0) {
+            result = 'buzz';
+            createLi('buzz', result);
+        }
+        else{
+            result = i;
+            createLi(' ', result);
+        }
+    }
 }
-
+const createLi = (cssClass, content) =>{
+    element = document.createElement("li");
+    element.setAttribute('class', cssClass);
+    element.textContent = content;
+    liste.insertAdjacentElement("beforeend", element);
+}
 document.querySelector('#fizzbuzz').remove();
-
 let liste=document.createElement('ol');
-
+console.log(mainTitle, liste);
 liste.setAttribute('id','fizzbuzz');
 liste.setAttribute('class','grid');
-const grid = mainTitle.insertAdjacentElement("afterend",liste);
-
-for (let i = 0; i < 100; i++) {
-    if (i%3 === 0 && i%5 === 0) {
-        result = 'fizzbuzz';
-        createLi('fizzbuzz', result);
-    }
-    else if (i%3 === 0) {
-        result = 'fizz';
-        createLi('fizz', result);
-    }
-    else if (i%5 === 0) {
-        result = 'buzz';
-        createLi('buzz', result);
-    }
-    else{
-        result = i;
-        createLi('grid', result);
+mainTitle.insertAdjacentElement("afterend",liste);
+verifNbre(1,100);
+window.onscroll = function() {
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+        verifNbre(start ,down )
+        start = down;
+        down+=20;
     }
 }
-
